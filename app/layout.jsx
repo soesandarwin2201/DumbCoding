4d0e7557eecdbd592cf83e13c41e1d0d345c061f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-25 10:40:11 +0630

    Generate the google id

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Children } from 'react';
 import Nav from '@/components/Nav';
+import Provider from '@/components/Provider';
 
 export const metadata = {
      title: 'DumbCoding',
@@ -11,6 +12,7 @@ const RootLayout = ({children}) => {
      return (
           <html lang='en'>
                <body>
+                    <Provider>
                     <div className="main">
                          <div className="gradient" />
                     </div>
@@ -18,6 +20,7 @@ const RootLayout = ({children}) => {
                          <Nav />
                          {children}
                     </main>
+                    </Provider>
                </body>
           </html>
      )
