e4c5475083cfa0a6d9892175ea80d9d6dbdf364e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-04 22:10:34 +0630

    add the callback function

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
 import { Schema, model, models } from 'mongoose';
 
 const userSchema = new Schema({
-     username: {
-          type: String,
-          require: [true, 'Username is required!'],
-          match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
-     },
-       email: {
-         type: String,
-         required: [true, 'Email already exists!'],
-         unique: [true,'Email is required!'],
-       },
-       image: {
-          type: String,
-       }
-     });
+  email: {
+    type: String,
+    required: [true, 'Email is required!'],
+    unique: [true, 'Email already exists!']
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String
+  }
+});
 
 const User = models.User || model("User", userSchema);
 
