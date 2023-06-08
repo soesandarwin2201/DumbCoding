1b4844d477567a3b26f52355a4bd2fae58f6281e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-08 10:19:12 +0630

    fix the errors

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: [true, 'Username is required!'],
-    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
   },
   image: {
     type: String
